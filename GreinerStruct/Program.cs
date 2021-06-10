bc53bcd27da5ee7967953d31ae36394b65631004
Flauschig <deineMudda@asshole.de>
2021-06-10 22:01:46 +0200

    Projektdateien hinzufügen.

---
+++ GreinerStruct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace GreinerStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(XmlElement));
+            XmlElement myElement =
+            new XmlDocument().CreateElement("root", "ns");
+            myElement.InnerText = "Hello World";
+            TextWriter writer = new StreamWriter("sex.lsd");
+            ser.Serialize(writer, new FetterSack("hallo"));
+            writer.Close();
+
+        }
+
+        [XmlRoot("PurchaseOrder", Namespace = "http://www.cpandl.com/",
+IsNullable = false)]
+        public record FetterSack(string Greiner);
+    }
+}
---
+++ GreinerStruct/XmlWriter/XmlRootWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GreinerStruct.XmlWriter
+{
+    class XmlRootWriter
+    {
+
+        private readonly XmlElement _xml;
+
+        public XmlRootWriter()
+        {
+            XmlElement myElement = new XmlDocument().CreateElement("root", "ns");
+        }
+
+    }
+}
