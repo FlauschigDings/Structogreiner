4365762d0c98eee3cede9b1367757bc56d915b39
unknown <unknown>
2021-06-15 15:27:08 +0200

    among us

--- GreinerStruct/Program.cs
+++ GreinerStruct/Program.cs
@@ -22,8 +22,7 @@ namespace GreinerStruct
 
         private static async Task Main(string[] args)
         {
-            var a = Console.ReadKey();
-            var i18n = new GreinerLanguage();
+            i18n = new GreinerLanguage();
             using var arrz = new ArrzFile();
             var ser = new XmlSerializer(typeof(XmlElement));
             using var writer = new StreamWriter("sex.nsd");
