ac8fd63bb0e3dc51b1944723a13ac0260f6a2126
unknown <unknown>
2021-06-16 17:22:24 +0200

    mehr Greiner

---
+++ GreinerStruct/Xml/Objects/Inline/Instruction.cs
@@ -0,0 +1,7 @@
+﻿namespace GreinerStruct.Xml.Objects.Inline
+{
+    internal class Instruction : Inline
+    {
+        public Instruction(string input) : base("instruction", input) { }
+    }
+}
--- GreinerStruct/xml/Objects/ControlStructures/Switch.cs
+++ GreinerStruct/xml/Objects/ControlStructures/Switch.cs
@@ -1,9 +1,4 @@
-﻿using GreinerStruct.Xml;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace GreinerStruct.Xml.Objects.ControlStructures
 {
